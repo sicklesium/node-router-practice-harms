68af8fb323caede6793f545f4f738b45761dad30
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-03-18 00:12:33 -0400

    GET and POST working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const server = express();
+server.use(express.json())
 server.use(cors());
 
 
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const server = require('./api/server');
 const port = 5000;
 
+const quotesRouter = require('./quotes/quotes-router.js')
+
+server.use("/api/quotes", quotesRouter)
+
+server.listen(port, () => {
+    console.log('server running on port 5000!')
+})
+
+module.exports = server;
