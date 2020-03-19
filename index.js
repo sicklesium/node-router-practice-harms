3e8f82071f2393fbd0892420f0a2aa30bae8608d
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-03-19 00:04:36 -0400

    PORT updated

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const server = require('./api/server');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const quotesRouter = require('./quotes/quotes-router.js')
 
 server.use("/api/quotes", quotesRouter)
 
 server.listen(port, () => {
-    console.log('server running on port 5000!')
+    console.log(`server running on port ${port}!`)
 })
 
 module.exports = server;
